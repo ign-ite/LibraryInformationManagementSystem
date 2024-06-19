e246377b30af60a3605c71cef650bfea1f930ac5
Varun Kumar <varuntheace@gmail.com>
2024-06-19 17:44:18 +0530

    Updated Database!!

--- scripts/database.py
+++ scripts/database.py
@@ -4,6 +4,7 @@ import datetime
 import os
 from book import *
 
+
 class Database:
     # Intilizing the Database Object (Null in this case)
     def __init__(self):
@@ -61,3 +62,205 @@ class Database:
             return False
 
         return inp
+
+    # This method is used to print the book details from the books file.
+    def print_it(self, data):
+        try:
+            print('\t\t\t\tID            : ', data[0])
+            print('\t\t\t\tName          : ', data[1])
+            print('\t\t\t\tAuthor        : ', data[2])
+            print('\t\t\t\tPublisher     : ', data[3])
+            print('\t\t\t\tPublish Date  : ', data[4])
+            print('\t\t\t\tAvailability  : ', data[5])
+            print('\t\t\t\tNo. of Copies : ', data[6])
+            print('\t', '-' * 110)
+        except IndexError:
+            pass
+        except Exception:
+            pass
+
+    # This method is used to validate the student details entered
+    def authenticate(self, stud_id, stud_password):
+        status = False
+        from student import Student
+        with open('../data/students.csv', mode='r', encoding='utf-8') as file:
+            csv_file = csv.reader(file)
+            for lines in csv_file:
+                try:
+                    if lines[0] == stud_id and lines[2] == stud_password:
+                        status = Student(lines[0], lines[1], lines[2], lines[3])
+                        break
+                except IndexError:
+                    pass
+                except Exception:
+                    pass
+
+        return status
+
+    # This method is used to fetch the last student id which is used to generate new id.
+    def fetch_last_student_id(self):
+        last = []
+        with open('../data/students.csv', mode='r', encoding='utf-8') as file:
+            csv_file = csv.reader(file)
+            lines = []
+            for lines in csv_file:
+                if len(lines) > 0:
+                    last = lines
+            try:
+                return lines[0]
+            except IndexError:
+                return last[0]
+            except Exception:
+                pass
+
+    # This is used to store the student details in students file
+    def save_student(self, stud_obj):
+        data = [stud_obj.student_id, stud_obj.student_name, stud_obj.student_password, stud_obj.student_batch]
+        with open('../data/students.csv', newline='', mode='a', encoding='utf-8') as file:
+            writer_object = csv.writer(file)
+            writer_object.writerow(data)
+
+    # This method allows thee student to borrow a book.
+    def borrow_book(self, stud_obj, book_id):
+        with open('../data/books.csv', mode='r', encoding='utf-8') as file:
+            books = csv.reader(file)
+            success = False
+
+            with open('../data/temp.csv', mode='a', newline='', encoding='utf-8') as temp_file:
+                for book in books:
+                    try:
+                        if book[0] == book_id:
+                            if int(book[6]) >= 1:
+                                book[6] = str(int(book[6]) - 1)
+                                if book[6] == 0:
+                                    book[5] = "False"
+                                success = book
+                    except IndexError:
+                        continue
+                    except Exception:
+                        continue
+
+                    temp_books = csv.writer(temp_file)
+                    temp_books.writerow(book)
+
+        os.remove('../data/books.csv')
+        os.rename('../data/temp.csv', '../data/books.csv')
+
+        if success:
+            book_obj = Book(success[0], success[1], success[2], success[3], success[4], success[5], success[6])
+            trans_id, today_date = self.write_to_transaction_and_borrow_file(stud_obj, book_obj, 'b')
+            return trans_id, today_date, book_obj
+
+        return success
+
+    # This method write to a file which track the borrow history.
+    def update_all_borrows_file(self, stud_obj, book_obj):
+        status = False
+
+        with open('../data/temp.csv', mode='w', newline='', encoding='utf-8') as temp_file:
+            temp_file_writer = csv.writer(temp_file)
+
+            with open('../data/all_borrows.csv', mode='r', encoding='utf-8') as borrow_file:
+                borrow_file_reader = csv.reader(borrow_file)
+
+                for line in borrow_file_reader:
+                    try:
+                        if line[1] == stud_obj.student_id and line[2] == book_obj.book_id and not status:
+                            status = True
+                            continue
+                    except IndexError:
+                        continue
+                    except Exception:
+                        continue
+                    temp_file_writer.writerow(line)
+
+        os.remove('../data/all_borrows.csv')
+        os.rename('../data/temp.csv', '../data/all_borrows.csv')
+
+    # This method allows the student to return a book.
+    def return_book(self, stud_obj, book_id):
+        with open('../data/books.csv', mode='r', encoding='utf-8') as file:
+            books = csv.reader(file)
+            success = False
+
+            with open('../data/temp.csv', mode='a', newline='', encoding='utf-8') as temp_file:
+                for book in books:
+                    try:
+                        if book[0] == book_id and not success:
+                            book[6] = str(int(book[6]) + 1)
+                            book[5] = "True"
+                            success = book
+                    except IndexError:
+                        continue
+                    except Exception:
+                        continue
+                    temp_books = csv.writer(temp_file)
+                    temp_books.writerow(book)
+
+        os.remove('../data/books.csv')
+        os.rename('../data/temp.csv', '../data/books.csv')
+
+        if success:
+            book_obj = Book(success[0], success[1], success[2], success[3], success[4], success[5], success[6])
+            trans_id = self.write_to_transaction_and_borrow_file(stud_obj, book_obj, 'r')
+            self.update_all_borrows_file(stud_obj, book_obj)
+
+            return trans_id, book_obj
+        return success
+
+    # This methods gets the last transaction id of a borrow or return.
+    def fetch_last_transaction_id(self):
+        last = []
+        lines = []
+        with open('../data/all_transactions.csv', mode='r', encoding='utf-8') as file:
+            csv_file = csv.reader(file)
+            for lines in csv_file:
+                if len(lines) > 0:
+                    last = lines
+            try:
+                return lines[0]
+            except IndexError:
+                return last[0]
+
+    # This method generates current date.
+    def get_current_date(self):
+        x = datetime.datetime.now()
+        return x.strftime("%d-%m-%Y")
+
+    # This method writes borrows and returns to the all_transaction file.
+    def write_to_transaction_and_borrow_file(self, stud_obj, book_obj, trans_type):
+        transaction_id = self.create_transaction_id()
+        today_date = self.get_current_date()
+
+        with open('../data/all_transactions.csv', mode='a', newline='', encoding='utf-8') as file:
+            data_to_write = [transaction_id, stud_obj.student_id, book_obj.book_id, today_date, trans_type]
+            temp_books = csv.writer(file)
+            temp_books.writerow(data_to_write)
+
+        if trans_type == 'b':
+            with open('../data/all_borrows.csv', mode='a', newline='', encoding='utf-8') as borrow_file:
+                data_to_write = [transaction_id, stud_obj.student_id, book_obj.book_id, today_date]
+                borrow_csv_writer = csv.writer(borrow_file)
+                borrow_csv_writer.writerow(data_to_write)
+
+        return transaction_id, today_date
+
+    # This method creates a new transaction id.
+    def create_transaction_id(self):
+        num_of_zeros = 4
+        try:
+            returned_id = self.fetch_last_transaction_id()
+            new_id = int(returned_id[2:]) + 1
+            temp = new_id
+            num_of_digits = 0
+
+            while temp > 0:
+                num_of_digits += 1
+                temp //= 10
+
+            new_id = 'tr' + '0' * (num_of_zeros - num_of_digits) + str(new_id)
+
+        except IndexError:
+            new_id = 'tr0001'
+
+        return new_id
